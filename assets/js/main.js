6712da8600a3f69ede342b0422f8d75819c6b6bb
RashidaAvshar <rashidaavshar@gmail.com>
2023-01-31 16:06:28 +0400

    add js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+document.querySelector("#add").addEventListener("click", ()=>{
+    const input = document.querySelector("#input").value;
+    console.log(input)
+    if(input.trim() == ""){
+        alert("please, write a task")
+    }else{
+        document.querySelector("#task-list").innerHTML += `<div class="list">
+        <span>${input}</span>
+        <button id="delete">remove</button>
+    </div>`
+    document.querySelector("#input").value = "";
+
+    const removeTask = document.querySelectorAll("#delete");
+    for(let i = 0; i< removeTask.length; i++){
+        removeTask[i].onclick = function(){
+            removeTask[i].parentNode.remove();
+        }
+    }
+    }
+})
